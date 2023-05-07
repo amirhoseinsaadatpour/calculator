8d098092e12ccbaf95ca05ee30515633d576337d
amirhoseinsaadatpour <saadatpouramirhosein@gmail.com>
2023-05-07 09:44:06 +0330

    refeactore  my code

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ function slashone()
 {
     document.getElementById("result").value = 1/document.getElementById("result").value;
 }
-// function delete() {
+function radikal()
+{
+    document.getElementById("result").value =  Math.sqrt(document.getElementById("result").value);
+}
+function deletee() {
+    var s = Array.from(document.getElementById("result").value);
+    s.pop();
+    document.getElementById("result").value = s
 
-// }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Calculator';
+// }
+// function display(value) {
+//  document.getElementById("result").innerText =+ value;
+// }
+// function clearscreen() {
+//   document.getElementById("result").innerText = "";
+// }
+// function calculat() {
+//   var p =document.getElementById("result").innerText;
+//   var q =eval(p);
+//   document.getElementById("result").innerText = q;
+// }
+// function slashone()
+// {
+//   document.getElementById("result").innerHTML = 1/document.getElementById("result").innerText;
+// }
+// function radikal()
+// {
+//   document.getElementById("result").value =  Math.sqrt(document.getElementById("result").value);
+// }
+// function deletee() {
+//   var s = Array.from(document.getElementById("result").value);
+//   s.pop();
+//   document.getElementById("result").value = s
+// }
